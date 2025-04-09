39ce32d7f4b5a78b10f43e9a631a4c9523f55d8f
AdrianGzn <233318@ids.upchiapas.edu.mx>
2025-04-09 14:03:30 -0500

    complementación de juego de contar

--- firstProject/firstProject/settings.py
+++ firstProject/firstProject/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'py',
         'USER': 'root',
-        'PASSWORD': 'root', 
+        'PASSWORD': '1234', 
         'HOST': '127.0.0.1', 
         'PORT': '3306'
     }
